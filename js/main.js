03eb7c47ae2830ba11061beb77ff51fbcc25198a
arnau.viladrich <arnauviladrich@innovamat.com>
2022-03-31 19:57:27 +0200

    new changes

--- js/main.js
+++ js/main.js
@@ -78,5 +78,5 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 6000); // Change image every 4 seconds
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
